2a5900fac491e69ed72ebad6a5de9a0031cbb75d
decide1415 <191705932@qq.com>
2017-09-27 16:38:06 +0800

    0927

--- src/com/logic/login.java
+++ src/com/logic/login.java
@@ -11,38 +11,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by decide on 2017/9/27.
  */
-public class login extends HttpServlet{
+public class login extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            String name = req.getParameter("name");
-            String Password = req.getParameter("Password");
-            Session session = HibernateUtils.getSession();
+        String name = req.getParameter("name");
+        String Password = req.getParameter("Password");
+        Session session = HibernateUtils.getSession();
 
         Query query = session.createQuery("from TdUser where TdUser .name = ? and TdUser .password = ?");
-        query.setString(0,name);
-        query.setString(1,Password);
-       List<TdUser> userList =  query.list();
-       if(userList!=null||userList.size()>=1){
+        query.setString(0, name);
+        query.setString(1, Password);
+        List<TdUser> userList = query.list();
+        if (userList != null || userList.size() >= 1) {
             //查询到用户数据
-           TdUser user  = userList.get(0);
-           req.getSession().setAttribute("USER",user);
-           resp.sendRedirect("/Page/main.jsp");
+            TdUser user = userList.get(0);
+            req.getSession().setAttribute("USER", user);
+            resp.sendRedirect("/Page/main.jsp");
 
-       }else {
+        } else {
             //未查询到用户数据处理
-           req.setAttribute("ERR",true);
-           req.getRequestDispatcher("login.jsp").forward(req,resp);
+            req.setAttribute("ERR", true);
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
 
-       }
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doGet(req,resp);
+        this.doGet(req, resp);
     }
 }
